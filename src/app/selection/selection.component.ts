818ac541e41cd3c64c674d743d2bb18141d5956f
jsshnik <js.saba999@gmail.com>
2022-02-13 19:09:14 -0800

    fixed

--- src/app/selection/selection.component.ts
+++ src/app/selection/selection.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selection.component.scss']
 })
 export class SelectionComponent implements OnInit {
-
+  classicArr = [1,2,3,4,5,6,7,8,9,10];
+  silverArr = [1,2,3,4,5,6,7,8,9,10]
   constructor() { }
 
   ngOnInit(): void {
